e77e636b33c75d647100a2bb2c0eebe7ec747107
matanume <matanume@users.noreply.github.com>
2023-09-20 19:39:24 +0700

    Update RecyclerListView.tsx to able to refresh with window anchor

--- src/core/RecyclerListView.tsx
+++ src/core/RecyclerListView.tsx
@@ -512,7 +512,12 @@ export default class RecyclerListView<P extends RecyclerListViewProps, S extends
             if (newProps.layoutProvider.shouldRefreshWithAnchoring) {
                 this._virtualRenderer.refreshWithAnchor();
             } else {
-                this._virtualRenderer.refresh();
+                let wc = this._getWindowCorrection(0,0,this.props);
+                if (wc.startCorrection + wc.windowShift > 0){
+                    this._virtualRenderer.refresh(wc);
+                } else {
+                    this._virtualRenderer.refresh();
+                }
             }
             this._refreshViewability();
         } else if (this.props.dataProvider !== newProps.dataProvider) {
